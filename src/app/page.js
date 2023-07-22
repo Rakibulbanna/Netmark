876807f1e697685b02c3512e16085b0c6b5e27cc
Rakib ul banna <banna@elitbuzz-bd.com>
2023-07-23 00:56:03 +0600

    Expandable component done

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,41 @@
-import Image from 'next/image'
+import FAQSection from '../../components/FAQSection';
 
+const faqData = [
+  {
+    header: 'What is Expandable ?',
+    content_body: 'The Expandable Lorem ipsum zsxr dcftvgybhuji kolawse drftgyh ujikol wsedrft gyhujiko dolor sit amet, consectetur adipisicing elit. Illo, voluptatibus aut est deleniti accusamus vel. Reprehenderit, facilis maxime. Illo, quae quam! Ad harum explicabo reiciendis minima minus incidunt, quasi, accusantium aliquam quisquam maiores earum dignissimos officia saepe consequuntur quia tempora nihil aperiam numquam dolore culpa nemo suscipit temporibus. Tempore quasi dolor vitae beatae reprehenderit deserunt asperiores ut recusandae, non fugit quo culpa velit modi adipisci rerum mollitia deleniti sequi, eligendi blanditiis quisquam dolorem cumque quod. Obcaecati fuga labore minima assumenda, facere sed quis illum blanditiis architecto veritatis similique quas perspiciatis id nesciunt soluta, dolores vero rerum dignissimos a eveniet sapiente. component is a collapsible and expandable component used for FAQ sections or any other scenario where you want to provide collapsible and expandable content.',
+    icon: {
+      collapse: "v",
+      expand: "^"
+    }
+  },
+  {
+    header: 'How can you use the Expandable component?',
+    content_body: 'You  can use the component by providing a header, an icon (optional), and the body content. When the header is clicked, the body content will be expanded or collapsed.',
+
+  },
+  {
+    header: 'How can you dvfbghgn weretrhtj adsfghn dfbgnhg ewrebgnr  fw ?',
+    content_body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde saepe provident beatae officiis esse? Nulla, explicabo inventore. Quae nemo, libero voluptatum veritatis harum et a. Pariatur, veniam quaerat? Deserunt quidem voluptates placeat reprehenderit, dicta repellendus sapiente. Natus exercitationem eveniet saepe explicabo dolorem, animi excepturi vel consequatur cupiditate fuga, sunt voluptatibus. (optional), and the body content. When the header is clicked, the body content will be expanded or collapsed.',
+
+  },
+  {
+    header: 'How canmponent?',
+  },
+];
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+
+    <div className=' grid grid-cols-1'>
+      {/* <div>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero incidunt itaque, veniam similique minus quas perspiciatis asperiores inventore sit animi pariatur alias! Rem animi ipsum ad mollitia laboriosam veniam assumenda.
+        </div> */}
+
+      <div className='p-4'>
+        <FAQSection faqData={faqData} />
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   )
 }
